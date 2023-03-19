a2703f00dd6beb39436f707666063184fdfa6a4b
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2023-03-19 17:26:08 -0300

    experiencia do usuario

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const form = document.querySelector("form");
 const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
 
+var senha = document.getElementById("password").value;
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   if (usernameInput.value === "" || passwordInput.value === "") {
-    alert("Por favor, preencha todos os campos.");
+    document.querySelector(".error-message").textContent =
+      "Preencha todos os campos";
     return;
   }
   if (passwordInput.value.length < 6) {
-    alert("A senha deve ter pelo menos 6 caracteres.");
+    document.querySelector(".error-message").textContent =
+      "A senha deve ter no mínimo 6 caracteres.";
     return;
   }
-  if (
-    !passwordInput.value.match(
-      /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/
-    )
-  ) {
-    alert(
-      "A senha deve conter pelo menos uma letra maiúscula, um número e um símbolo."
-    );
+  if (!passwordInput.value.match(/^(?=.*\d)/)) {
+    document.querySelector(".error-message").textContent =
+      "A senha deve conter pelo menos um numero.";
     return;
   }
   // Se chegamos até aqui, os campos foram preenchidos corretamente
@@ -29,4 +28,3 @@ form.addEventListener("submit", function (event) {
   localStorage.setItem("authToken", token);
   window.location.href = "criptografia.html";
 });
-
